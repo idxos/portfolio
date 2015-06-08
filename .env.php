5301bd009c1f1053aea234c915ab585475156af4
Christopher J. Roberts <croberts@lknpro.com>
2015-06-08 15:57:38 -0400

    adding .env stuff

--- .env.php
+++ .env.php
@@ -1,6 +1,6 @@
 <?php return array (
-  'DB_HOST' => '45.55.237.156',
   'DB_PASSWORD' => 'mxUnvwDPcQVyPuX3AwWp',
   'DB_NAME' => 'october',
   'DB_USERNAME' => 'forge',
+  'DB_HOST' => '45.55.237.156',
 );
